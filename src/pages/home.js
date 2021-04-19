53bf47d2b54a0f8bd7504b4624fc8886e835aa52
Manuel Duran <durbonca@gmail.com>
2021-04-19 15:42:32 -0400

    bulding feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,18 +4,21 @@ import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer } from '../containers/footer'
 import { FaqsContainer } from '../containers/faq'
 import { HeaderContainer } from '../containers/header'
-import { OptForm } from '../components'
+import { Features, OptForm } from '../components'
 
 export default function Home () {
     return(
         <>
-            <HeaderContainer>   
+            <HeaderContainer>
                 <OptForm>
-                    <OptForm.Input placeholder="email address" />
-                    <OptForm.Button >Try it now</OptForm.Button>
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart
-                    your membership.</OptForm.Text>
-                    <OptForm.Break/>
+                <Features>
+                    <Features.Title>Unlimited films, TV programmes and more.</Features.Title>
+                        <Features.Subtitle>Watch anywhere. Cancel at any time.</Features.Subtitle>
+                        <OptForm.Input placeholder="email address" />
+                        <OptForm.Button >Try it now</OptForm.Button>
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                        <OptForm.Break/>
+                    </Features>
                 </OptForm>
             </HeaderContainer>
             <JumbotronContainer/>
