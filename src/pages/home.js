208db0a7ad366b70f1df98abb62577edf8674ea8
Manuel Duran <durbonca@gmail.com>
2021-04-19 16:23:05 -0400

    styling feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,16 +10,16 @@ export default function Home () {
     return(
         <>
             <HeaderContainer>
-                <OptForm>
                 <Features>
                     <Features.Title>Unlimited films, TV programmes and more.</Features.Title>
-                        <Features.Subtitle>Watch anywhere. Cancel at any time.</Features.Subtitle>
+                    <Features.Subtitle>Watch anywhere. Cancel at any time.</Features.Subtitle>
+                    <OptForm>
                         <OptForm.Input placeholder="email address" />
                         <OptForm.Button >Try it now</OptForm.Button>
                         <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
                         <OptForm.Break/>
-                    </Features>
-                </OptForm>
+                    </OptForm>
+                </Features>
             </HeaderContainer>
             <JumbotronContainer/>
             <FaqsContainer/>
