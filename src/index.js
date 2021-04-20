61079d3366b10dbfac735a70965f4e52162536db
Manuel Duran <durbonca@gmail.com>
2021-04-20 09:05:36 -0400

    hooking aplication with firebase

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles'
 import App from './App';
+import { FirebaseContext } from './context/firebase'
 
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+}
+
+const firebase = window.firebase.initializeApp(config)
 
 ReactDOM.render(
+
   <React.StrictMode>
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+
+    </FirebaseContext.Provider>
     <GlobalStyles/>
       <App />
   </React.StrictMode>,
+
   document.getElementById('root')
 );
