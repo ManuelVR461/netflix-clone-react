817f18bfed4487ac6a1302d151d5885898f189a9
Manuel Duran <durbonca@gmail.com>
2021-04-22 15:11:07 -0400

    adding card item

---
+++ src/components/card/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Group} from './styles/card'
+
+export default function Card () {
+    return( <></> )
+}
+
+Card.Group = function CardGroup ({children, ... restProps}){
+   return <Group></Group>
+}
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BrowseContainer} from '../containers/browse'
 import {useContent} from '../hooks'
+import { selectionMap } from '../utils'
+
 
 export default function Browse (){
     const {series} = useContent('series')
     const {films} = useContent('films')
 
-    console.log(series)
-    console.log(films)
+    const slides = selectionMap({series, films})
 
-    return <BrowseContainer/>
+    return <BrowseContainer slides={slides}/>
 }
